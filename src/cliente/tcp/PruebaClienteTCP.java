53c6b09c1f07054fda5ea2262b0d926157c76553
Gerardo Jimenez Arguelles <33142689@uaz.edu.mx>
2021-05-12 16:52:51 -0500

    first commit

---
+++ src/cliente/tcp/PruebaClienteTCP.java
@@ -0,0 +1,9 @@
+package cliente.tcp;
+
+public class PruebaClienteTCP{
+    public static void main(String args[])throws Exception{
+        ClienteTCP clienteTCP =new ClienteTCP("192.168.100.3",60000);
+             
+        clienteTCP.inicia();
+    }
+}
